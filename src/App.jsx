cf21c5a34aa6580a4853396aaece2b568d7090d3
Abhishek <abhi.anandwade@gmail.com>
2024-04-17 23:11:41 +0530

    Refactor file imports and add new components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import { Routes, Route } from "react-router-dom";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="app">
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/order' element={<PlaceOrder/>}/>
+        </Routes>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,12 +1,37 @@
-import React, { createContext } from "react";
-import PropTypes from 'prop-types'; // Import PropTypes
+import React, { createContext, useEffect } from "react";
+
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+
+    const [cartItems, setCartItems] = React.useState([]);
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev)=>({...prev,[itemId]:1}));
+        }
+        else{
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
+
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        addToCart,
+        removeFromCart,
+        setCartItems
     };
 
     return (
@@ -16,8 +41,5 @@ const StoreContextProvider = (props) => {
     );
 };
 
-StoreContextProvider.propTypes = {
-    children: PropTypes.node, // Add PropTypes validation for children
-};
-
+ 
 export default StoreContextProvider;
