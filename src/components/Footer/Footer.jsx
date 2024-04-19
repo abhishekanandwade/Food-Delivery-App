7c355bedaf1d40721f5bc06a078a308f2cade909
Abhishek <abhi.anandwade@gmail.com>
2024-04-19 22:18:35 +0530

    Refactor file imports, add new components and pages, and update styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
           <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="" />
             <img src={assets.twitter_icon} alt="" />
-            <img src={assets.instagram_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
           </div>
         </div>
         <div className="footer-content-center">
