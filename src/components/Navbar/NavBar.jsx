bce2b4680f17d7349521e044e7ee493c5098017b
Abhishek <abhi.anandwade@gmail.com>
2024-04-30 16:22:19 +0530

    Refactor NavBar component, update logo link, and add checkout button in Cart page

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = ({setShowLogin}) => {
 
   return (
     <div className="navbar">
-      <Link to='/home'><img src={assets.logo} alt="logo" className="logo" /></Link>
+      <Link to='/'><img src={assets.logo} alt="logo" className="logo" /></Link>
       <ul className="navbar-menu">
         <Link to='/' onClick={()=>setMenu("home")} className={menu=="home" ? "active" : ""}>home</Link>
         <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu=="menu" ? "active" : ""}>menu</a>
